c40a80d5d38f5e8c2baf1b3ef546f992c1e122a4
Baylor Rae <bweathers@mtrade.com>
2017-11-21 14:02:03 -0600

    target multiple net47 and netstandard2.0

---
+++ src/Let.cs/LetHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace LetTestHelper
+{
+    public static class LetHelper
+    {
+        private static Dictionary<string, object> _objects = new Dictionary<string, object>();
+
+        public static T Let<T>(string description, Func<T> func)
+        {
+            if (_objects.ContainsKey(description))
+                return (T) _objects[description];
+
+            var result = func();
+            _objects.Add(description, result);
+            return result;
+        }
+
+        public static T Let<T>(Expression<Func<T>> expression)
+        {
+            var expressionBody = expression.Body.ToString();
+            var genericArguments = expression.ReturnType.GenericTypeArguments;
+
+            if (genericArguments.Length > 0)
+                expressionBody += string.Join(",", genericArguments.Select(t => t.Name));
+
+            if (_objects.ContainsKey(expressionBody))
+                return (T) _objects[expressionBody];
+
+            var result = expression.Compile().Invoke();
+            _objects.Add(expressionBody, result);
+            return (T) result;
+        }
+
+        public static void Flush()
+        {
+            _objects.Clear();
+        }
+
+        private class Defined { }
+    }
+}
---
+++ tests/Let.cs.Tests/LetHelperTest.cs
@@ -0,0 +1,111 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using LetTestHelper;
+
+namespace Let.cs.Tests
+{
+    [TestFixture]
+    public class LetHelperTest
+    {
+        private Mock<Spy> _spy;
+        private Func<bool> _callback;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _spy = new Mock<Spy>();
+            _callback = () => LetHelper.Let(() => _spy.Object.Called());
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            LetHelper.Flush();
+        }
+
+        [Test]
+        public void ItCachesTheResult()
+        {
+            Func<int> notSoRandom = () => LetHelper.Let(() => new Random().Next(10000));
+            var firstValue = notSoRandom();
+
+            for (var i = 0; i < 10; i++)
+                Assert.That(notSoRandom(), Is.EqualTo(firstValue));
+        }
+
+        [Test]
+        public void ItInvokesTheExpressionOnce()
+        {
+            for (var i = 0; i < 10; i++)
+                _callback();
+
+            _spy.Verify(s => s.Called(), Times.Once);
+        }
+
+        [Test]
+        public void ItClearsTheCachedResults()
+        {
+            _callback();
+            _callback();
+
+            LetHelper.Flush();
+
+            _callback();
+            _callback();
+
+            _spy.Verify(s => s.Called(), Times.Exactly(2));
+        }
+
+        [Test]
+        public void ItAllowsMultipleTypesToBeStored()
+        {
+            Func<string> callbackString = () => LetHelper.Let(() => "string");
+            Func<int> callbackInteger = () => LetHelper.Let(() => 1337);
+
+            Assert.That(callbackString(), Is.EqualTo("string"));
+            Assert.That(callbackInteger(), Is.EqualTo(1337));
+        }
+
+        [Test]
+        public void ItAllowsGenericsToBeAssigned()
+        {
+            Mock<I1> WrappedI1() => LetHelper.Let(() => new Mock<I1>());
+            Mock<I2> WrappedI2() => LetHelper.Let(() => new Mock<I2>());
+
+            Assert.That(WrappedI1().Object, Is.Not.EqualTo(WrappedI2().Object));
+        }
+            
+        [Test]
+        public void NamedInstancesWithFuncs()
+        {
+            Spy Spy1() => LetHelper.Let("spy1", () => new Spy());
+            Spy Spy2() => LetHelper.Let("spy2", () => new Spy());
+
+            Assert.That(Spy1(), Is.Not.EqualTo(Spy2()));
+        }
+
+        [Test, Ignore("should this throw an exception or overwrite the previous definition?")]
+        public void ItDoesntAllowNamingCollisions()
+        {
+            Spy Spy1() => LetHelper.Let("spy1", () => new Spy());
+            Spy Spy2() => LetHelper.Let("spy1", () => new Spy());
+
+            // option 1
+            Assert.That(Spy2, Throws.InvalidOperationException);
+
+            // option 2
+            // this feels wrong but it's also a
+            // very simple example
+            Assert.That(Spy1(), Is.EqualTo(Spy2()));
+        }
+
+        public class Spy
+        {
+            public virtual bool Called() { return true; }
+        }
+
+        public interface I1 { }
+        public interface I2 { }
+    }
+}
